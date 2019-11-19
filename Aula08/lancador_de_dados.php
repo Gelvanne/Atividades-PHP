ad0f6fe380986962c859a799138b575c164ae86a
Gelvanne AR Oliveira <57466731+Gelvanne@users.noreply.github.com>
2019-11-19 10:55:08 -0300

    Update and rename Aula08/lancador_de_dados.php to  Aula08/lancador_de_dados.php

--- Aula08/lancador_de_dados.php
+++ Aula08/lancador_de_dados.php
@@ -1,12 +1,34 @@
 <? php $metodo = $_SERVER["REQUEST_METHOD"];?>
 <? php if ($metodo == "GET") { ?>
-<!DOCTYPE htlm>
 
-<html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Lançador de dados  PHP</title>
-</head>
-<body>
-</body>
+	<!DOCTYPE htlm>
+
+	<html>
+
+	<head>
+	<meta charset="ISO-8859-1">
+	<title>Lançador de dados  PHP</title>
+
+	</head>
+
+	<body>
+	<br>
+	<label> Seja bem vindo a Aula 08 - Atividade 03</label>
+	<br><hr>
+	<form method="POST">
+	<label>Digite o valor mínimo:</label> 	<input name="minimo"/>
+	<label>Digite o valor máximo:</label> 	<input name="maximo"/>
+						<input type="submit"/>
+	</form>
+
+<? php } ?>
+
+<? php else { ?>
+	$min = $_POST["minimo"];
+	$max = $_POST["maximo"];
+	$resultado = rand( $min, $max );
+	echo $resultado;
+<? php } ?>
+	</body>
+
 </html>
