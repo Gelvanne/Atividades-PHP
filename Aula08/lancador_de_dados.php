8616d69194dc51e3a1fc9c62dbc49fbc6e068867
Gelvanne AR Oliveira <57466731+Gelvanne@users.noreply.github.com>
2019-11-19 10:19:45 -0300

    Update and rename Aula08/lancador_de_dados.php to  Aula08/lancador_de_dados.php

--- Aula08/lancador_de_dados.php
+++ Aula08/lancador_de_dados.php
@@ -1,1 +1,12 @@
+<? php $metodo = $_SERVER["REQUEST_METHOD"];?>
+<? php if ($metodo == "GET") { ?>
+<!DOCTYPE htlm>
 
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Lançador de dados  PHP</title>
+</head>
+<body>
+</body>
+</html>
